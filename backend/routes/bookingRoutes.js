18b048230405d5d39deb7f076823b61b4d6fbe35
pavanvadapalli <pavanvadapalli205@gmail.com>
2026-07-05 13:13:51 +0530

    chore: clean up temporary diagnostic-all route

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -5,19 +5,6 @@ const { protect } = require('../middleware/authMiddleware');
 
 router.post('/', createBooking);
 router.get('/', protect, getBookings);
-
-const Booking = require('../models/Booking');
-const User = require('../models/User');
-router.get('/diagnostic-all', async (req, res) => {
-  try {
-    const bookings = await Booking.find({});
-    const users = await User.find({});
-    res.json({ bookings, users });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
 router.put('/:id', protect, updateBooking);
 
 module.exports = router;
